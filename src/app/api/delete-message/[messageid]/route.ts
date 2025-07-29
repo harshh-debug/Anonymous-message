8a65a58f4aa259aa7277557c1a9a1d56068cfa11
harshh-debug <harshj9705@gmail.com>
2025-07-29 23:49:26 +0530

    error fixed

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -2,32 +2,33 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import dbconnect from "@/lib/dbconnect";
 import UserModel from "@/models/User";
-import {User} from "next-auth"
+import { User } from "next-auth";
 
-
-export async function DELETE(request: Request,{ params }: { params: { messageid: string } }) {
+export async function DELETE(
+    request: Request,
+    { params }: { params: Promise<{ messageid: string }> }
+) {
     const { messageid } = await params;
     const messageId = messageid;
     await dbconnect();
 
-    const session = await getServerSession(authOptions)
-    const user:User = session?.user as User
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
 
-    if(!session || !session.user) {
+    if (!session || !session.user) {
         return Response.json({
             success: false,
             message: "Not authenticated"
-        }, { status: 401 }
-        );
+        }, { status: 401 });
     }
 
     try {
-        const updatedResult=await UserModel.updateOne(
-            {_id: user._id},
-            {$pull: { messages: {_id: messageId} }}
+        const updatedResult = await UserModel.updateOne(
+            { _id: user._id },
+            { $pull: { messages: { _id: messageId } } }
+        );
 
-        )
-        if(updatedResult.modifiedCount === 0) {
+        if (updatedResult.modifiedCount === 0) {
             return Response.json({
                 success: false,
                 message: "Message not found or already deleted"
@@ -39,12 +40,10 @@ export async function DELETE(request: Request,{ params }: { params: { messageid:
             message: "Message deleted successfully"
         }, { status: 200 });
     } catch (error) {
-        console.error("Error deleting message route :", error);
+        console.error("Error deleting message route:", error);
         return Response.json({
-            success: false, 
+            success: false,
             message: "Failed to delete message"
         }, { status: 500 });
-        
     }
-    
 }
